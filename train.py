ad02ba60c2fec06ef01a2ba43d10283fef3f6a1d
Opinderjit Bhella <opinderjit.bhella@hp.com>
2021-04-14 15:41:36 -0700

    updated ...

--- train.py
+++ train.py
@@ -3,10 +3,11 @@ from sklearn.linear_model import LinearRegression
 import pickle
 import os
 
-X, y = make_regression(10000,n_features = 10)
+X, y = make_regression(10000, n_features = 11)
 
 # Train a model
 reg = LinearRegression().fit(X, y.ravel())
+
 # Print out training r2
 print(reg.score(X,y.ravel() ))
 
